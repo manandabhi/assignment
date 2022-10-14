53cc7e58df524cbc2d9cf539967a886420db6562
mr.malaviya_11 <113505057+Mrmalaviya11@users.noreply.github.com>
2022-10-14 10:26:29 +0530

    m

---
+++ module 3.1/find area of triangle.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int height,base;
+	float area;
+	
+	printf("Enter height of triangle\n");
+	scanf("%i",&height);
+	
+	printf("Enter base of triangle\n");
+	scanf("%i",&base);
+	
+	area=0.5*height*base;
+	
+	printf("area of triangle=%.2f",area);
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
